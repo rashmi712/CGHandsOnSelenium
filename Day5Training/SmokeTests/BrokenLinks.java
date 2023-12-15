f1867d19d1f5e68adac7bfe972d97fa90185eb1f
rashmi712 <rashmi712@gmail.com>
2023-12-15 14:25:49 +0530

    Create BrokenLinks.java

---
+++ Day5Training/SmokeTests/BrokenLinks.java
@@ -0,0 +1,63 @@
+package SmokeTests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import FunctionalTests.OpenBrowser;
+
+//import org.openqa.selenium.chrome.ChromeDriver;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+
+public class BrokenLinks {
+
+
+public static void main(String[] args) {
+//System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
+//WebDriver driver = new ChromeDriver();
+
+OpenBrowser ob = new OpenBrowser();
+WebDriver driver = ob.launchChrome();
+driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+//driver.get("https://demowebshop.tricentis.com/");
+driver.get("file:///C:/Users/rsingh80/Desktop/Sample.html");
+
+
+// Finding all the available links on webpage
+List<WebElement> links = driver.findElements(By.tagName("a"));
+
+
+// Iterating each link and checking the response status
+for (WebElement link : links) {
+String url = link.getAttribute("href");
+verifyLink(url);
+}
+
+
+driver.quit();
+}
+
+
+public static void verifyLink(String url) {
+try {
+URL link = new URL(url);
+HttpURLConnection httpURLConnection = (HttpURLConnection) link.openConnection();
+httpURLConnection.setConnectTimeout(3000); // Set connection timeout to 3 seconds
+httpURLConnection.connect();
+
+
+if (httpURLConnection.getResponseCode() == 200) {
+System.out.println(url + " - " + httpURLConnection.getResponseMessage());
+} else {
+System.out.println(url + " - " + httpURLConnection.getResponseMessage() + " - " + "is a broken link");
+}
+} catch (Exception e) {
+System.out.println(url + " - " + "is a broken link");
+}
+}
+}
