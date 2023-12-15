6a8aae5840f5a93f299248cc7064309718c30571
rashmi712 <rashmi712@gmail.com>
2023-12-15 14:20:36 +0530

    Create AllTests.java

---
+++ Day1Selenium/Day1Test/AllTests.java
@@ -0,0 +1,11 @@
+package TestTypes;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({Day1TestCases.test_Calculator_add_int_int.class,Day1TestCases.test_Scientific_Calculator.class})
+
+public class AllTests {
+
+}
